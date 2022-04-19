a718b3aceda51cad5def76c4820bbd0b239f069a
Farid <farid.ibrahim29@gmail.com>
2022-04-19 15:01:15 -0400

    My hotelProject second

---
+++ src/test/java/stepDefinitions/TestStepDef.java
@@ -0,0 +1,37 @@
+package stepDefinitions;
+
+import org.junit.Assert;
+
+import core.Base;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import pageObjects.HotelHomePageObject;
+
+public class TestStepDef extends Base {
+	
+	HotelHomePageObject hotelHomePage = new HotelHomePageObject(); 
+	@Given("user is on hotel website")
+	public void user_is_on_hotel_website() {
+	    String actualTitle = driver.getTitle(); 
+	    String expectedTitle = "Hotel"; 
+	    Assert.assertEquals(expectedTitle, actualTitle);
+	    logger.info(actualTitle + " is equal to " + expectedTitle); 
+	}
+	@When("user click on signIn button")
+	public void user_click_on_sign_in_button() {
+		hotelHomePage.clickSignIn();
+		logger.info("user clicked on signIn");
+		
+	 
+	}
+	@Then("user should see signIn page")
+	public void user_should_see_sign_in_page() {
+		Assert.assertTrue(hotelHomePage.isAuthenticationDisplayed());
+		Assert.assertTrue(hotelHomePage.isEmailPresent());
+		logger.info("Sign in is displayed");
+	    
+
+}
+}
